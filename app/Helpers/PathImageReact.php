1d31de419a8d31f6f204f6c559489b9504ddbc70
HarunUmar <arunumar21@gmail.com>
2020-11-07 08:00:03 +0800

    update 7-11-20 from local agogo

--- app/Helpers/PathImageReact.php
+++ app/Helpers/PathImageReact.php
@@ -6,7 +6,7 @@ class PathImageReact {
 	public static function getPath($resolusi)
 	{
 
-		 $path_image = "http://192.168.1.30/agogo/upload/images-".$resolusi."/";
+		 $path_image = "http://192.168.1.30/agogo/upload/";
 
 		 //$path_image = "http://agogobakery.com/atur/upload/images-".$resolusi."/";
 
