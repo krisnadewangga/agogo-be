7650cc105c3ce09f2abf127c6c396290b7f7908e
fajrin_si2011 <fajrin_si2011@mahasiswa.ung.ac.id>
2021-06-04 14:34:49 +0800

    update transaksi api

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -33,7 +33,7 @@ class Transaksi extends Model
                  'jenis',
                  'jalur',
                  'kasir_id',
-                 'for_ps','waktu_kirim_tf','top_up'];
+                 'for_ps','waktu_kirim_tf','top_up','opsi_bt','biaya_admin'];
 
     protected $dates = ['waktu_kirim','tgl_bayar','waktu_kirim_tf'];
 
