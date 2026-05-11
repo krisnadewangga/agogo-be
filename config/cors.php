48381ace52b2c7285d26bbd121a061c485e35bfb
krisnadewangga <krisnadewanggapp@gmail.com>
2026-05-11 12:48:05 +0700

    Remove debug routes from CORS middleware paths

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*', 'login', 'logout', 'debug-*'],
+    'paths' => ['api/*', 'login', 'logout'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
